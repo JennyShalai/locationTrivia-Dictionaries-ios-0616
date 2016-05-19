05802b4c98302b582931c6d95aff4e1de8d6c744
JennyShalai <jenny.shalai@gmail.com>
2016-05-19 16:03:19 -0400

    OMG I'm done with Objective-C track

--- locationTrivia-Dictionaries/FISAppDelegate.h
+++ locationTrivia-Dictionaries/FISAppDelegate.h
@@ -5,11 +5,15 @@
 @interface FISAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+ 
+- (NSString *)stringByTruncatingNameOfLocation:(NSDictionary *)location toLength:(NSUInteger)length;
+ 
+- (NSDictionary *)dictionaryForLocationWithName:(NSString *)name latitude:(CGFloat)latitude longitude:(CGFloat)longitude;
 
-/**
+- (NSArray *)namesOfLocations:(NSArray *)locations;
+
+- (BOOL)dictionaryIsValidLocation:(NSDictionary *)dictionary;
  
- *  Declare your method names here.
- 
- */
+- (NSDictionary *)locationNamed:(NSString *)name inLocations:(NSArray *)locations;
 
 @end
--- locationTrivia-Dictionaries/FISAppDelegate.m
+++ locationTrivia-Dictionaries/FISAppDelegate.m
@@ -10,10 +10,54 @@
     return YES;
 }
 
-/** 
- 
- *  Write your method implementations here.
- 
- */
+- (NSString *)stringByTruncatingNameOfLocation:(NSDictionary *)location toLength:(NSUInteger)length {
+    // returns a shortened location name
+    NSString *name = location[@"name"];
+    if ( [name length] > length ) {
+        name = [name substringToIndex:length];
+        return name;
+    } else {
+        return name;
+    }
+}
+
+- (NSDictionary *)dictionaryForLocationWithName:(NSString *)name latitude:(CGFloat)latitude longitude:(CGFloat)longitude {
+    NSMutableDictionary *newDictionary = [[NSMutableDictionary alloc] init];
+    [newDictionary setValue:name forKey:@"name"];
+    [newDictionary setValue:@(latitude) forKey:@"latitude"];
+    [newDictionary setValue:@(longitude) forKey:@"longitude"];
+    return newDictionary;
+}
+
+- (NSArray *)namesOfLocations:(NSArray *)locations {
+    NSMutableArray *arrayOfLocations = [[NSMutableArray alloc] init];
+    for (NSDictionary *location in locations) {
+        NSString *nameOfLocation = location[@"name"];
+        [arrayOfLocations addObject:nameOfLocation];
+    }
+    return arrayOfLocations;
+}
+
+- (BOOL)dictionaryIsValidLocation:(NSDictionary *)dictionary {
+    NSArray *keys = [dictionary allKeys];
+    if ([keys count] != 3) {
+        return false;
+    } else if ([keys containsObject: @"name"] && [keys containsObject: @"latitude"] && [keys containsObject: @"longitude"]) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+- (NSDictionary *)locationNamed:(NSString *)name inLocations:(NSArray *)locations {
+    for (NSDictionary *location in locations) {
+        if ([location[@"name"] isEqual:name]) {
+            return location;
+        }
+    }
+    return nil;
+}
+
+
 
 @end
